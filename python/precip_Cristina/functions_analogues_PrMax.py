1a18dc9e3dc936930b2db8819785d8bebc4afd28
aportal0 <alice.portal94@gmail.com>
2025-08-21 08:38:21 +0200

    Update 2 21st August 2025

--- python/precip_Cristina/functions_analogues_PrMax.py
+++ python/precip_Cristina/functions_analogues_PrMax.py
@@ -11,6 +11,8 @@ import matplotlib.colors as mcolors
 import time
 from collections import defaultdict
 import xesmf as xe
+import cftime
+from datetime import datetime
 
 
 ## Functions to load data from ERA5 and CERRA
@@ -23,7 +25,7 @@ def load_ERA5_data(varname, freq, timestep, lonlat_bounds, l_anom):
     if freq == 'daily':
         timestep = (timestep.normalize() + pd.Timedelta(hours=9)).isoformat()
     # Possible varnames: 'mslp', 'z500'
-    data_dir = '/work_big/users/clima/portal/ERA5/'+varname+'/'
+    data_dir = '/media/alice/Crucial X9/portal/data_CNR/ERA5/'+varname+'/'
     if l_anom:
         files = [os.path.join(data_dir, f'ERA5_{varname}_NH_{freq}_{year}_anom.nc') for year in years_data]
     else:
@@ -49,7 +51,8 @@ def load_ERA5_clim(varname, doy, lonlat_bounds, l_smoothing):
     """Loads ERA5 climatology 1985-2019 for a given variable and day-of-year (doy) in lonlat_bounds.
     l_smoothing = True for 31-day smoothing time window."""
     # Possible varnames: 'msl'
-    data_dir = '/work_big/users/clima/portal/ERA5/'+varname+'/climatology/'
+    # data_dir = '/work_big/users/clima/portal/ERA5/'+varname+'/climatology/'
+    data_dir = '/media/alice/Crucial X9/portal/data_CNR/ERA5/'+varname+'/climatology/'
     if l_smoothing:
         file = os.path.join(data_dir, f'ERA5_{varname}_NH_daily_clim_2004-2023_sm31d.nc')
     else:
@@ -68,11 +71,11 @@ def load_ERA5_clim(varname, doy, lonlat_bounds, l_smoothing):
     data = data.where(mask, np.nan).dropna(dim=str_lat, how="all").dropna(dim=str_lon, how="all")
     return data
 
-def load_CERRA_data(varname, timestep, lonlat_bounds):
+def load_CERRA_italy_data(varname, timestep, lonlat_bounds):
     """Loads CERRA data for a given variable, timestep and in lonlat_bounds.
     Timestep can be a single datetime object or a list of datetime objects."""
     # Possible varnames: 'precip'
-    data_dir = '/work_big/users/clima/zappa/CERRA-LAND/daily/nc/regular/'
+    data_dir = '/mnt/naszappa/CERRA/daily/nc/regular/'
     years_data = np.unique(timestep.strftime("%Y"))
     files = [os.path.join(data_dir, f'precip_{year}_italy_reg10.nc') for year in years_data]
     # select variable and timestep
@@ -148,6 +151,179 @@ def get_anomaly_climatology_paths_CRCM5(CRCM5_dir, varname, list_membs, year_ran
     return memb_files, memb_files_clim
 
 
+def get_anomaly_climatology_paths_CRCM5_bymonth(CRCM5_dir, varname, list_membs, list_times):
+    """
+    Return anomaly and climatology file paths for a specific variable and epoch (year_range).
+    
+    Parameters:
+        CRCM5_dir (str): Base directory for the files.
+        varname (str): Variable name (e.g., 'tas').
+        list_membs (list): List of member names (e.g., ['r1i1p1', 'r2i1p1']).
+        list_times (list): List of lists containing times for each member, where each time can be a string or datetime object.
+
+    Returns:
+        tuple: (memb_files, memb_files_clim) where each is a dict {member: [file_paths]}.
+    """
+    # File directory
+    dir_files = CRCM5_dir + varname + '/'
+    # Initialize a list to hold the file paths
+    paths_files = []
+    paths_files_clim = []
+    prefix_files = f"{varname}-anom" if varname != "zg" else f"{varname}500-anom"
+    
+    for im, member in enumerate(list_membs):
+        
+        times = list_times[im]
+        
+        for time in times:  
+            
+            # Check if time is a string or datetime object
+            if isinstance(time, cftime.DatetimeNoLeap):
+                time = datetime(time.year, time.month, time.day, hour=0, minute=0, second=0)
+            elif isinstance(time, str):
+                time = datetime.strptime(time, "%Y-%m-%d  %H:%M:%S")
+            elif isinstance(time, datetime):
+                pass
+            else:
+                raise ValueError("Time must be a string or datetime object.")
+            
+            # Extract year and month from the time
+            year = time.year
+            month = time.month
+            year_range = get_year_range(year)
+            
+            # Create labels for the file names
+            time_label = f"{year:04d}{month:02d}"
+            suffix_file = f"_{time_label}_remapbil-to-05res.nc"
+            suffix_files_clim = f"{month:02d}_clim{year_range[0]}-{year_range[1]}_sm31d_05res.nc"
+            
+            # Create the file name
+            file_dir = f"{dir_files}{member}/{year}/res05/"
+            if os.path.exists(file_dir):
+                for file in os.listdir(file_dir):
+                    file_path = os.path.join(file_dir, file)
+                    if os.path.isfile(file_path) and file.startswith(prefix_files) and file.endswith(suffix_file):
+                        paths_files.append(file_path)
+            
+            # Create the climatology file name
+            file_clim_dir = f"{dir_files}{member}/clim/"
+            if os.path.exists(file_clim_dir):
+                for file in os.listdir(file_clim_dir):
+                    file_path = os.path.join(file_clim_dir, file)
+                    if os.path.isfile(file_path) and file.endswith(suffix_files_clim):
+                        paths_files_clim.append(file_path)
+    
+    # Organize by member
+    paths_files = list(set(paths_files))
+    memb_files = create_member_file_dict(paths_files, list_membs)
+    paths_files_clim = list(set(paths_files_clim))
+    memb_files_clim = create_member_file_dict(paths_files_clim, list_membs)
+
+    # Sort the paths
+    for memb in memb_files_clim:
+        memb_files_clim[memb].sort()
+    for memb in memb_files:
+        memb_files[memb].sort()
+
+    return memb_files, memb_files_clim
+
+
+def get_precipitation_paths_CRCM5(CRCM5_dir, list_membs, year_range):
+    """
+    Return anomaly and climatology file paths for a specific variable and epoch (year_range).
+    
+    Parameters:
+        CRCM5_dir (str): Base directory for the files.
+        list_membs (list): List of member names (e.g., ['kba', 'kbb']).
+        year_range (tuple): Year range as a tuple (start_year, end_year).
+    Returns:
+        dict: A dictionary where keys are member names and values are lists of file paths.
+    """
+    varname = 'pr'
+    # Define the prefix for files based on year range
+    if year_range[1] < 2006:
+        prefix_files = [f"{varname}_daysum_historical_"]
+    elif year_range[0] >= 2006:
+        prefix_files = [f"{varname}_daysum_rcp85_"]
+    else:
+        prefix_files = [f"{varname}_daysum_historical_", f"{varname}_daysum_rcp85_"]
+
+    # Build file directory
+    dir_files = CRCM5_dir + varname + '/'
+
+    # Collect files
+    paths_files = []
+    if os.path.exists(dir_files):
+        for file in os.listdir(dir_files):
+            file_path = os.path.join(dir_files, file)
+            if (
+                os.path.isfile(file_path)
+                and file.startswith(tuple(prefix_files))
+                # and any(file.startswith(prefix) for prefix in prefix_files)
+                and any(member in file for member in list_membs)
+            ):
+                paths_files.append(file_path)
+
+    # Organize by member
+    memb_files = create_member_file_dict(paths_files, list_membs)
+    # Sort the paths
+    for memb in memb_files:
+        memb_files[memb].sort()
+
+    return memb_files
+
+
+def get_precipitation_paths_CRCM5_bymonth(CRCM5_dir, list_membs, list_times):
+    """
+    Return anomaly and climatology file paths for a specific variable and epoch (year_range).
+    
+    Parameters:
+        CRCM5_dir (str): Base directory for the files.
+        list_membs (list): List of member names (e.g., ['kba', 'kbb']).
+        list_times (list): List of lists containing times for each member, where each time can be a string or datetime object.
+    Returns:
+        dict: A dictionary where keys are member names and values are lists of file paths.
+    """
+    varname = 'pr'
+    # File directory
+    dir_files = CRCM5_dir + varname + '/'
+    # Initialize a list to hold the file paths
+    paths_files = []
+    for im, member in enumerate(list_membs):
+        times = list_times[im]
+        for time in times:  
+            # Check if time is a string or datetime object
+            if isinstance(time, cftime.DatetimeNoLeap):
+                time = datetime(time.year, time.month, time.day, hour=0, minute=0, second=0)
+            elif isinstance(time, str):
+                time = datetime.strptime(time, "%Y-%m-%d  %H:%M:%S")
+            elif isinstance(time, datetime):
+                pass
+            else:
+                raise ValueError("Time must be a string or datetime object.")
+            # Extract year and month from the time
+            year = time.year
+            month = time.month
+            # Create labels for the file names
+            if year < 2006:
+                simulation = 'historical'
+            else:
+                simulation = 'rcp85'
+            time_label = f"{year:04d}{month:02d}"
+            # Create the file name
+            file = f"{dir_files}{member}/{year}/{varname}_daysum_{simulation}_{member}_remapbil-to-05res_{time_label}.nc"
+            # Append the file path to the list
+            paths_files.append(file)
+
+    # Organize by member
+    memb_files = create_member_file_dict(paths_files, list_membs)
+    # Sort the paths
+    for memb in memb_files:
+        memb_files[memb].sort()
+
+    return memb_files
+
+
 def create_member_file_dict(paths_files, list_membs):
     """
     Create a dictionary mapping members to their respective file paths.
@@ -165,7 +341,9 @@ def create_member_file_dict(paths_files, list_membs):
             if memb in path:
                 memb_files[memb].append(path)
                 break  # Assuming one member per file path
-    return memb_files
+    # return with keys in alphabetical order
+    return {m: memb_files[m] for m in sorted(list_membs)}
+
 
 def open_member_datasets(memb_files_dict, combine, expand_member_dim):
     """
@@ -195,6 +373,15 @@ def open_member_datasets(memb_files_dict, combine, expand_member_dim):
     return list_ds
 
 
+def get_year_range(year):
+    """
+    Get the year range for a given year.
+    """
+    ranges = [(1955, 1974), (2004, 2023), (2030, 2049), (2080, 2099)]
+    year_range = next(([start, end] for start, end in ranges if start <= year <= end), None)
+    return year_range
+
+
 ## Function to create lon-lat mask (event-wise)
 
 def box_event_PrMax_alertregions(no_node, no_event):
@@ -214,6 +401,25 @@ def lonlat_mask(lon, lat, lonlat_bounds):
     lon_mask = (lon >= lonlat_bounds[0]) & (lon <= lonlat_bounds[1])
     lat_mask = (lat >= lonlat_bounds[2]) & (lat <= lonlat_bounds[3])
     return lon_mask, lat_mask
+
+## Function to retrieve best model analogue info 
+def get_best_model_analogue_info(no_node, no_event, var_analogues):
+    """
+    Returns the best model analogue info for a given selecion of node and event.
+    
+    Parameters:
+        l_sel (str): Selection type (e.g., 'Italy', 'wide-region', 'alert-regions').
+        no_node (int): Node number.
+        no_event (int): Event number.
+    
+    Returns:
+        dictionary with "member", "distance" and "date" of the best model analogue.
+    """
+    if no_node==1 and no_event==1:
+        if var_analogues == 'psl':
+            dict_best_analogue = {'member': 'kbw', 'distance': 125.24177, 'date': '2019-11-25'}
+    
+    return dict_best_analogue
 
 
 ## Function to regrid data using xESMF
@@ -278,7 +484,7 @@ def plot_geopotential_and_mslp(ax, timestep, lonlat_bounds, z500, msl):
     ax.set_ylabel('Latitude')
 
 
-def plot_precipitation(ax, timestep, lonlat_bounds, precip):
+def plot_precipitation(ax, timestep, lonlat_bounds, precip, precip_levels):
     """Plots the precipitation data for a given timestep."""
 
     # Create grid 
@@ -295,25 +501,18 @@ def plot_precipitation(ax, timestep, lonlat_bounds, precip):
     gl.right_labels = False
     gl.top_labels = False
     # Parameters for colormap
-    levels = np.arange(0, 210, 20)
     cmap = plt.get_cmap("YlGnBu")
-    norm = mcolors.BoundaryNorm(boundaries=levels, ncolors=cmap.N, extend='max')
+    norm = mcolors.BoundaryNorm(boundaries=precip_levels, ncolors=cmap.N, extend='max')
     # Precipitation
     mesh = ax.pcolormesh(lon1, lat, precip, transform=ccrs.PlateCarree(), cmap=cmap, norm=norm)
-    cbar = plt.colorbar(mesh, ax=ax, orientation='vertical', pad=0.05, boundaries=levels, ticks=levels, extend='max')
+    cbar = plt.colorbar(mesh, ax=ax, orientation='vertical', pad=0.05, boundaries=precip_levels, ticks=precip_levels, extend='max')
     cbar.set_label('24h precipitation [mm]')
     # Title and labels
     ax.set_title(f"Timestep: {timestep}")
 
 
-def plot_anom_event(varname, lon, lat, anom_event, clim):
+def plot_anom_event(ax, varname, lon, lat, anom_event, clim):
     """Plots the anomaly and DOY climatology for a given event (mslp or z500)."""
-
-    # Create a plot with Cartopy
-    fig, ax = plt.subplots(
-        figsize=(12, 8),
-        subplot_kw={"projection": ccrs.PlateCarree()}
-    )
 
     # Set intervals and levels
     if varname == 'z500':
@@ -344,5 +543,5 @@ def plot_anom_event(varname, lon, lat, anom_event, clim):
         cbar_label = "$\\Delta$Z500 (m)"
     elif varname == 'mslp':
         cbar_label = "$\\Delta$mslp (hPa)"
-    cbar = fig.colorbar(cf, ax=ax, shrink=0.6, label=cbar_label)
-    return fig, ax
+    plt.colorbar(cf, ax=ax, shrink=0.6, label=cbar_label)
+    return ax
