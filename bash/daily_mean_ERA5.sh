3a6465af481ba989452653769cb44290463b4a4f
Alice Portal <portal@tintin.bo.isac.cnr.it>
2025-09-26 10:33:34 +0200

    Update 26 Sept 2025

--- bash/daily_mean_ERA5.sh
+++ bash/daily_mean_ERA5.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Variable
-namevar='mslp'
+namevar='z500'
 # Input and output directories
-INPUT_DIR="/work_big/users/portal/ERA5/${namevar}/"
-OUTPUT_DIR="/work_big/users/portal/ERA5/${namevar}/"
+INPUT_DIR="/work_big/users/clima/portal/ERA5/${namevar}/"
+OUTPUT_DIR="/work_big/users/clima/portal/ERA5/${namevar}/"
 mkdir -p "$OUTPUT_DIR"  # Ensure output directory exists
 
 # Loop through years
-for year in {1986..2023}; do
-    if [[ $namevar == 'z500' ]]; then 
-	    input_file0="${INPUT_DIR}ERA5_${namevar}_NH_6hr_${year}.grib"
-    fi
+for year in {2024..2024}; do
+#    if [[ $namevar == 'z500' ]]; then 
+#	    input_file0="${INPUT_DIR}ERA5_${namevar}_NH_6hr_${year}.grib"
+#    fi
     input_file="${INPUT_DIR}ERA5_${namevar}_NH_6hr_${year}.nc"
     output_file="${OUTPUT_DIR}ERA5_${namevar}_NH_daily_${year}.nc"
    
@@ -19,7 +19,7 @@ for year in {1986..2023}; do
     if [[ $namevar == 'z500' || $namevar == 'mslp' ]]; then
 	    cdo daymean -del29feb "$input_file" "$output_file"
 	    rm -f $input_file0
-    elif [ $namevar == 'msl' ]; then
+    elif [ $namevar == 'mslp' ]; then
 	    cdo daymean -del29feb -sellonlatbox,0,360,0,90 "$input_file" "$output_file"
     fi
      
